252e9d37364afab5a4efde64181e14bd3fbdcdf0
Long Zheng <long.zheng@gmail.com>
2024-04-11 15:05:01 +1000

    Ignore dist

--- dist/main.js
+++
@@ -1,5 +0,0 @@
-export * from "./xmp-fonts";
-export * from "./xmp-ids";
-export * from "./xmp-markers";
-export * from "./xmp-thumbnails";
-export * from "./xmp";
--- dist/xmp-ids.d.ts
+++
@@ -1,4 +0,0 @@
-export interface IXmpID {
-    prefix: string;
-    id: string;
-}
